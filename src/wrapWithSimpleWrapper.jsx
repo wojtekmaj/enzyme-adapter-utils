2e0927b455ad8c93cff8df4c618bde8b85f1a206
Wojciech Maj <kontakt@wojtekmaj.pl>
2021-03-24 12:27:45 +0100

    Drop support for React <17

--- src/wrapWithSimpleWrapper.jsx
+++ src/wrapWithSimpleWrapper.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { intersects } from 'semver';
 import { or, explicitNull } from 'airbnb-prop-types';
 import PropTypes from 'prop-types';
 
@@ -11,23 +10,10 @@ const defaultProps = {
   children: undefined,
 };
 
-const Wrapper = (intersects('>= 0.14', React.version)
-  // eslint-disable-next-line prefer-arrow-callback
-  ? () => Object.assign(function SimpleSFCWrapper({ children }) {
-    return children;
-  }, { propTypes, defaultProps })
-  : () => {
-    class SimpleClassWrapper extends React.Component {
-      render() {
-        const { children } = this.props;
-        return children;
-      }
-    }
-    SimpleClassWrapper.propTypes = propTypes;
-    SimpleClassWrapper.defaultProps = defaultProps;
-    return SimpleClassWrapper;
-  }
-)();
+// eslint-disable-next-line prefer-arrow-callback
+const Wrapper = () => Object.assign(function SimpleSFCWrapper({ children }) {
+  return children;
+}, { propTypes, defaultProps });
 
 export default function wrap(element) {
   return <Wrapper>{element}</Wrapper>;
