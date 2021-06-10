28a54aec18127f1ce43323524b7a2693863d83c7
Henry Q. Dineen <henryqdineen@users.noreply.github.com>
2021-06-10 16:27:21 -0400

    Fix Wrapper variable type in wrapWithSimpleWrapper (#1)
    * fix wrapWithSimpleWrapper
    
    * fix ESLint prefer-arrow-callback error

--- src/wrapWithSimpleWrapper.jsx
+++ src/wrapWithSimpleWrapper.jsx
@@ -9,10 +9,7 @@ const defaultProps = {
   children: undefined,
 };
 
-// eslint-disable-next-line prefer-arrow-callback
-const Wrapper = () => Object.assign(function SimpleSFCWrapper({ children }) {
-  return children;
-}, { propTypes, defaultProps });
+const Wrapper = Object.assign(({ children }) => children, { propTypes, defaultProps });
 
 export default function wrap(element) {
   return <Wrapper>{element}</Wrapper>;
